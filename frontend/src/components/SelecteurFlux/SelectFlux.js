8122a9c70c8c785ff8e35b67f53099b435672904
Corentin88 <corentin.lanaud@wanadoo.fr>
2025-07-27 18:17:28 +0200

    amélio responsive sélecteur

--- frontend/src/components/SelecteurFlux/SelectFlux.js
+++ frontend/src/components/SelecteurFlux/SelectFlux.js
@@ -13,11 +13,11 @@ export default function SelectFlux({ value, onChange }) {
   
     return (
       <div className="mt-4">
-        <div className="w-[550px] px-4 py-2 mx-auto flex space-x-2 align-center justify-center bg-orange-50 rounded-sm">
+        <div className="w-max-[550px] px-4 py-2 mx-auto flex flex-col space-x-2 align-center justify-center bg-orange-50 rounded-sm sm:flex-row sm:w-[550px]">
           {types.map(type => (
             <button
               key={type}
-              className={`px-4 py-2 ${value === type ? 'border-b-4 border-orange-500 font-bold bg-orange-50 rounded-sm' : 'hover:bg-orange-300 rounded-sm hover:scale-105 transition-all duration-200 '}`}
+              className={`px-4 py-2 ${value === type ? 'border-b-4 border-orange-500 font-bold bg-orange-50 rounded-sm' : 'hover:bg-orange-200 rounded-sm hover:scale-105 transition-all duration-200 '}`}
               onClick={() => onChange(type)}
             >
               {type}
