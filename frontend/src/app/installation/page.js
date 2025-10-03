c8a8e7fcdf7c6a7912436161a6ba1641c1dd8707
Corentin88 <corentin.lanaud@wanadoo.fr>
2025-10-03 14:55:39 +0200

    Fix page installation

--- frontend/src/app/installation/page.js
+++ frontend/src/app/installation/page.js
@@ -9,8 +9,8 @@ export default function Installation() {
     return (
       <div className="mt-20 flex flex-col items-center justify-center w-full px-4">
         <h1>Installation</h1>
-        <p>En cours...</p>
-        <p>Vous pouvez lire README.md (front-end et back-end)</p>
+        <p>En cours de création...</p>
+        <p>Vous pouvez lire README.md (front-end et back-end(création d'un compte, gestion des flux)) sur Github</p>
       </div>
     );
 
